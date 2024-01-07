5eef2be4567cc717d12eaba321b761d780f67b32
Irene Wang <lohas1107@gmail.com>
2024-01-07 11:47:34 +0800

    refactor: extract module bmi

--- 02-unit-test/src/main.rs
+++ 02-unit-test/src/main.rs
@@ -1,22 +1,3 @@
-#[cfg(test)]
-mod bmi_test {
-    use crate::calculate;
-
-    #[test]
-    fn test_calculate() {
-        let result = calculate(180, 65);
-        assert_eq!(20.1, result);
-    }
-}
-
-fn calculate<T, U>(height: T, weight: U) -> f64
-where
-    T: Into<f64>,
-    U: Into<f64>,
-{
-    let h = height.into() / 100.0;
-    let bmi = weight.into() / (h * h);
-    (bmi * 10.0).round() / 10.0
-}
+mod bmi;
 
 fn main() {}
