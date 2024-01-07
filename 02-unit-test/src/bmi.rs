7f16e7a4bf16df65b461f54f944e777737b2c975
Irene Wang <lohas1107@gmail.com>
2024-01-07 17:24:59 +0800

    refactor: format bmi.rs

--- 02-unit-test/src/bmi.rs
+++ 02-unit-test/src/bmi.rs
@@ -1,7 +1,7 @@
 fn calculate<T, U>(height: T, weight: U) -> f64
-    where
-        T: Into<f64>,
-        U: Into<f64>,
+where
+    T: Into<f64>,
+    U: Into<f64>,
 {
     let h = height.into() / 100.0;
     let bmi = weight.into() / (h * h);
